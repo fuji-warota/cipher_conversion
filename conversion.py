2b4d7a13202a97916b695b157270554a598bbb3f
Fuji_warota <255116p@gs.kochi-tech.ac.jp>
2021-05-24 23:16:49 +0900

    aまたはzを超過した際に一周するように設定

--- conversion.py
+++ conversion.py
@@ -1,9 +1,14 @@
 print("Let's play at Caesar cipher!")
 
 tmp = list(input()) #変換対象文字列
-seed = int(input()) 
-rel = [0] * len(tmp)
-for i in range(len(tmp)):
-   rel[i] = chr(ord(tmp[i])+seed)
-
-print(*rel)
+print ("How many seeds?")
+seed = int(input())
+#for i in range(seed):
+ans = ""
+for word in list(tmp):
+  asc = ord(word)
+  foo = asc - 97
+  foo = (foo + seed) % 26
+  asc = foo + 97
+  ans += chr(asc)
+print(ans)
