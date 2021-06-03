62b7c8f4433d2f5cefb1b06bde40eab1406fc98f
Fuji_warota <255116p@gs.kochi-tech.ac.jp>
2021-06-03 19:47:44 +0900

    ひらがな対応に着手

--- conversion.py
+++ conversion.py
@@ -1,14 +1,21 @@
 print("Let's play at Caesar cipher!")
 
 tmp = list(input()) #変換対象文字列
-print ("How many seeds?")
+print("How many seeds?")
 seed = int(input())
 #for i in range(seed):
 ans = ""
 for word in list(tmp):
   asc = ord(word)
-  foo = asc - 97
-  foo = (foo + seed) % 26
-  asc = foo + 97
+  #文字種類を判別
+  if asc < 12354:
+    foo = asc - 97
+    foo = (foo + seed) % 26
+    asc = foo + 97
+  else:
+    foo = asc - 12354
+    foo = (foo + seed * 2) % 81
+    asc = foo + 12354
+    print(asc)
   ans += chr(asc)
 print(ans)
